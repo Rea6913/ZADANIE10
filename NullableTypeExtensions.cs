6f75d08e9e5a9709aad9d0d24b2294b3de332325
Rea6913 <47655248+Rea6913@users.noreply.github.com>
2019-03-01 15:02:59 +0300

    10

--- NullableTypeExtensions.cs
+++ NullableTypeExtensions.cs
@@ -7,7 +7,20 @@ namespace PadawansTask10
         public static bool IsNull(this object variable)
         {
             // put your code here
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            try
+            {
+                string a = (string)variable;
+                int b = (int)variable;
+                double c = (double)variable;
+                char d = (char)variable;
+            }
+            catch
+            {
+                if (variable!=null)
+                    return false;
+            }
+            return true;
         }
     }
 }
